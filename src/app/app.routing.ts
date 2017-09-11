efda4a416d981b86865e388ee2597bbde4126ac0
don79is <don79is@gmail.com>
2017-09-11 13:57:16 +0300

    add signin route component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,9 +2,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {AllUsersComponent} from './admin/users/all-users/all-users.component';
 import {AllPostsComponent} from './admin/posts/all-posts/all-posts.component';
 import {ModuleWithProviders} from '@angular/core';
+import {SigninComponent} from './admin/auth/signin/signin.component';
+
 
 
 const APP_ROUTES: Routes = [
+    {path: 'admin', component: SigninComponent},
     {path: 'admin/users', component: AllUsersComponent},
     {path: 'admin/dashboard', component: AllUsersComponent},
     {path: 'admin/posts', component: AllPostsComponent},
